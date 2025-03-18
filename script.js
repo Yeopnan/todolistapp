60a7bcf85ea406af292bd04865b4c48979c0a5fa
YeopNan <ayeopnan@gmail.com>
2025-03-18 11:38:00 +0800

    js

--- script.js
+++ script.js
@@ -8,5 +8,30 @@ function addtask() {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  inputBox.value = "";
+  saveData();
 }
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagname === "LI") {
+      e.target.classlist.toggle("checked");
+      saveData();
+    } else if (e.target.tagname === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showtask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
